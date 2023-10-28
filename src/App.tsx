6514e385452f433c933d5a62efd85df922a50842
Emilly Soares Santos <soaressantosemilly05@gmail.com>
2023-10-27 20:08:05 -0400

    Defining the inputs and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import React, { useEffect, useState } from "react";
+
 function App() {
 
+  const [author, setAuthor] = useState("");
+  const [text, setText] = useState("");
+  const [comments, setComments] = useState<string[]>([]);
+
   return (
-    <>
-      <p>oi</p>
-    </>
+    <div>
+      <h1>Feedbacks</h1>
+
+      <div>
+        <input type="text" placeholder="Autor" />
+      </div>
+
+      <div>
+        <input type="text" placeholder="Comentário" />
+      </div>
+
+      <button>
+        Comentar
+      </button>
+
+    </div>
   )
 }
 
