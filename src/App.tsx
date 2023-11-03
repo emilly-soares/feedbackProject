a3bed4d1a19706fdefb4668285662a0d99451491
Emilly Soares Santos <soaressantosemilly05@gmail.com>
2023-11-02 21:18:42 -0400

    adding comments in the code

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,44 @@ import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+  //useState (estados) para as contantes do autor, texto do comentário e para armazenar os comentários
   const [author, setAuthor] = useState("");
   const [text, setText] = useState("");
   const [comments, setComments] = useState<string[]>([]);
 
+  //useEffect (efeitos) para sempre carregar os comentários salvos na local storage
   useEffect(() => {
+    //Obter os comentários salvos na local storage
     const savedComments = localStorage.getItem("comments");
+    //Se tiver comentários na Local Storage atualiza o estado "comments" com os comentários recuperados.
     if (savedComments) {
       setComments(JSON.parse(savedComments));
     }
   }, []);
 
+  //Função acionada ao clicar no botão comentar
   function handleComment() {
+    //Verifica se o autor ou texto estão vazios
     if (!author || !text) return;
+
+    //Verifica se os dois estão preenchidos
     if (author && text) {
+      //Cria um novo comentário (newComment) com autor, texto e data/hora atual
       const newComment = `${new Date().toLocaleString()}):${author}:${text}`;
+      //Atualiza a lista de comentários e adiciona o novo comentário na parte superior
       setComments([newComment, ...comments]);
+      //Salva os comentários na local storage em formato JSON
       localStorage.setItem("comments", JSON.stringify([newComment, ...comments]));
+      //Limpa as inputs do autor e texto
       setAuthor("");
       setText("");
     }
-  }
 
+  }
+  //Retorna o formulário dos comentários
   return (
 
     <div className="feedback">
-
       <h1>Feedbacks</h1>
 
       <div>
@@ -61,9 +73,7 @@ function App() {
           ))}
         </section>
       </div>
-
-      <p>Total Comments: {comments.length}</p>
-
+      <p>Total Comentários: {comments.length} 💚</p>
     </div>
   );
 }
